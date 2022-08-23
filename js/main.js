d0bf0c9cc5641e3302bda16cad75d7d5584ce9ea
Tim Schiemann <tschiemann@unity-mail.de>
2022-08-23 21:06:57 +0200

    Create news section, start nav

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/* TOGGLE NAV  */
+const navToggle = document.querySelector('.nav__toggle')
+const nav = document.querySelector('.nav__list')
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible')
+})
