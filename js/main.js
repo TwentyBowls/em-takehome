fa99804b8487d5059476349470287801e9648062
Tim Schiemann <tschiemann@unity-mail.de>
2022-08-24 22:43:37 +0200

    I have no idea what I am doing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* TOGGLE NAV  */
 function toggleNav() {
-    const nav = document.querySelector('.nav__items')
+    const nav = document.querySelector('.nav')
     document.getElementById('nav-btn').addEventListener('click', () => {
         nav.classList.toggle('nav--open')
     })
@@ -12,12 +12,12 @@ toggleNav()
 
 /* TOGGLE SUBMENUS */
 
-function toggleSubmenu(){
-    const submenus = document.querySelectorAll('.nav__subcategory')
-    document.querySelectorAll('.nav__subcategory-btn').addEventListener('click', () => {
-        sub
-    })
-}
+// function toggleSubmenu(){
+//     const submenus = document.querySelectorAll('.nav__subcategory')
+//     const submenuBtns = document.querySelector('.nav__subcategory-btn')
+//     submenuBtns.map(btn => btn.addEventListener('click', () => console.log('test')))
+//     console.log(submenuBtns)
+// }
 
-toggleSubmenu()
+// toggleSubmenu()
 
