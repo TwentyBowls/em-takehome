02b0fb63829e42646dd34138f9436cff3e09ada6
Tim Schiemann <tschiemann@unity-mail.de>
2022-08-24 17:34:27 +0200

    Tweak styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 /* TOGGLE NAV  */
-document.addEventListener('DOMContentLoaded', () => {
+function toggleNav() {
     const nav = document.querySelector('.nav__items')
     document.getElementById('nav-btn').addEventListener('click', () => {
         nav.classList.toggle('nav--open')
     })
-})
+}
+
+toggleNav()
+
+
+
+/* TOGGLE SUBMENUS */
+
+function toggleSubmenu(){
+    const submenus = document.querySelectorAll('.nav__subcategory')
+    document.querySelectorAll('.nav__subcategory-btn').addEventListener('click', () => {
+        sub
+    })
+}
+
+toggleSubmenu()
+
